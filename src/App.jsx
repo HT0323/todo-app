d812e6c5098b71c25537801890360b257f4e059f
HaraT0323 <alra9si916323@gmail.com>
2022-03-08 22:40:56 +0900

    タスク作成機能の追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import React, { useState } from "react";
 import "./App.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('');
   const [incompleteTodos, setIncompleteTodos] = useState(['task1', 'task2']);
   const [completeTodos, setCompleteTodos] = useState(['task3', 'task4']);
+
+  const onChangeTodoText = (e) => {
+    setTodoText(e.target.value)
+  };
+
+  const onClickAdd = () => {
+    if (todoText === '') return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText('');
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}/>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
@@ -19,7 +32,7 @@ export const App = () => {
                 <li>{todo}</li>
                 <button>完了</button>
                 <button>削除</button>
-            </div>
+              </div>
             )
           })}
         </ul>
@@ -32,7 +45,7 @@ export const App = () => {
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>戻す</button>
-            </div>
+              </div>
             )
           })}
         </ul>
