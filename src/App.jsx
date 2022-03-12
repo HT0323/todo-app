3902b623545f89a0bfa18aef0099519d2aa6bab2
HaraT0323 <alra9si916323@gmail.com>
2022-03-12 17:32:45 +0900

    登録できるタスクの制限

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,13 @@ export const App = () => {
 
   return (
     <>
-      <InputTodo todoText = {todoText} onChange = {onChangeTodoText} onClick = {onClickAdd} />
+      <InputTodo
+        todoText = {todoText}
+        onChange = {onChangeTodoText}
+        onClick = {onClickAdd}
+        disabled = {incompleteTodos.length >= 5}
+      />
+      {incompleteTodos.length >= 5 && <p style={{ color: 'red' }}>登録できるtodoは5個まで</p>}
       <IncompleteTodos todos = {incompleteTodos} onClickComplete = {onClickComplete} onClickDelete = {onClickDelete} />
       <CompleteTodos todos = {completeTodos} onClickBack = {onClickBack}/>
     </>
--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 export const InputTodo = (props) => {
-   const {todoText, onChange, onClick} = props;
+  const {todoText, onChange, onClick, disabled} = props;
   return (
     <div className="input-area">
-      <input placeholder="TODOを入力" value={todoText} onChange={onChange}/>
-      <button onClick={onClick}>追加</button>
+      <input disabled={disabled} placeholder="TODOを入力" value={todoText} onChange={onChange} />
+      <button disabled={disabled} onClick={onClick}>追加</button>
     </div>
   )
 };
