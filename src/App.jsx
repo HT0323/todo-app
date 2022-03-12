75ad96edab03de8a22c1173d7b76e56d7fb592e0
HaraT0323 <alra9si916323@gmail.com>
2022-03-12 15:29:28 +0900

    タスク削除機能の追加

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,20 @@ export const App = () => {
   const onChangeTodoText = (e) => {
     setTodoText(e.target.value)
   };
-
+　
+  // タスクの追加機能
   const onClickAdd = () => {
     if (todoText === '') return;
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText('');
+  };
+
+  // タスクの削除機能
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
   };
 
   return (
@@ -26,12 +34,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             )
           })}
